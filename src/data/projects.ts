0186cf88f13289a15bf8582544c82a0ef64b61ba
onyxas <35199535+Zenixas@users.noreply.github.com>
2023-08-16 15:58:20 +0300

    small changes

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -4,33 +4,38 @@ export type Project = {
   link: string;
   description?: string;
   isComingSoon?: boolean;
+  website?: string;
 };
 
 const projects: Project[] = [
   {
-    isComingSoon: true,
+    isComingSoon: false,
     title: "KET Mobile App",
     techs: ["React", "Typescript", "html/css"],
-    link: "/",
-    description: "KET (Kelių Eismo Taisyklės) application is specifically developed for mobile platforms, offering a free and comprehensive platform for practicing KET-related questions and acquiring theoretical knowledge."
+    link: "https://www.youtube.com/watch?v=Nianw4Yztjs",
+    description: "KET (Kelių Eismo Taisyklės) application is specifically developed for mobile platforms, offering a free and comprehensive platform for practicing KET-related questions and acquiring theoretical knowledge.",
+    website: "youtube.com"
   },
   {
     title: "ZMINER",
     techs: ["C/C++", "C#", "Javascript", "html/css"],
     link: "https://blissful-northcutt-5d222e.netlify.app/",
     description: "Zminer is an intuitive and user-friendly Windows application for simplified cryptocurrency mining, making it easily accessible to all users.",
+    website: "netlify.app"
   },
   {
     title: "NetRenamer",
     techs: ["C#"],
     link: "https://github.com/Zenixas/NetRenamer",
-    description: "NET RENAMER is an open-source Windows application for renaming and obscuring various elements within a .NET module, including methods, fields, classes, and others"
+    description: "NET RENAMER is an open-source Windows application for renaming and obscuring various elements within a .NET module, including methods, fields, classes, and others",
+    website: "github.com"
   },
   {
     title: "Zenfuscator",
     techs: ["C#"],
     link: "https://github.com/Zenixas/ZenFuscator",
-    description: "ZenFuscator is an open-source obfuscation tool to safeguard .NET assemblies from reverse engineering efforts."
+    description: "ZenFuscator is an open-source obfuscation tool to safeguard .NET assemblies from reverse engineering efforts.",
+    website: "github.com"
   },
 ];
 
